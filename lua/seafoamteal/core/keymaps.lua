197c708789218f4e436eaf6bd639be27bd0062c3
Hari <hprasad2709@gmail.com>
2024-08-05 01:10:29 +0100

    Added conform.nvim

--- lua/seafoamteal/core/keymaps.lua
+++ lua/seafoamteal/core/keymaps.lua
@@ -6,7 +6,7 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-a>", { desc = "Decrement number" })
 
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertical" })
-keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Split horizontal" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontal" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { 
     desc = "Close current split" 
--- lua/seafoamteal/plugins/colorscheme.lua
+++ lua/seafoamteal/plugins/colorscheme.lua
@@ -3,6 +3,16 @@ return {
         name = "catppuccin",
         priority = 1000,
         config = function()
+            require("catppuccin").setup({
+                styles = {
+                    conditionals = {},
+                },
+                
+                integrations = {
+                    treesitter = true,
+                },
+            })
+
             vim.cmd("colorscheme catppuccin-mocha")
 
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
